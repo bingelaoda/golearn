0c0f171cfc0cae3e65e3b9e30f61377432d408e0
bingelaoda <1072466135@qq.com>
2018-11-07 21:22:40 +0800

    golearn first submit

---
+++ variabledefineanddatatype/FirstProgram.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+/**
+1 函数使用func修饰符修饰
+2 go程序的主入口函数是main函数
+3 类似于c语言里面也有格式化输入输出 需要导入fmt
+4 一般情况下运行需要在main包下进行
+
+ */
+func main()  {
+	//fmt.Println("a");
+	fmt.Print("nihao\nhello")
+}
